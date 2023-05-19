d79cb55b4ad48528b4e71aee8fbb8c37481c31e0
Vaughn Holmes <vaughnhlms@hotmail.com>
2023-05-19 17:11:44 -0400

    Added function to read data

--- src/lib.rs
+++ src/lib.rs
@@ -12,11 +12,18 @@ use std::fs::File;
 use std::path::Path;
 use std::string::FromUtf8Error;
 
+use ndarray::Array2;
+use nitf_2_1::image_segment::ImageSegment;
+use nitf_2_1::types::Segment;
+use num_complex::Complex32;
+
 pub mod nitf_2_1;
 
 use nitf_2_1::Nitf;
 
-
+/// Construct a `Nitf` object from a file `path`. 
+/// 
+/// If `path` is `None`, returns `Nitf::default()` 
 pub fn read_nitf(path: Option<&Path>) -> Result<Nitf, FromUtf8Error> {
     match path {
         Some(path) => {
@@ -25,6 +32,36 @@ pub fn read_nitf(path: Option<&Path>) -> Result<Nitf, FromUtf8Error> {
         }
         None => return Ok(Nitf::default())
     }
+}
+
+// TODO: Implement reading a slice of the data into an array, as opposed to the whole thing.
+/// Read image data from `image_header` into an array
+/// 
+/// Only supports `Complex32` data
+pub fn data_to_array(image_header: &Segment<ImageSegment, memmap2::Mmap>) -> Array2<Complex32> {
+    let n_row: usize = image_header.meta.NROWS.string.parse().unwrap();
+    let n_col: usize = image_header.meta.NCOLS.string.parse().unwrap();
+
+    let mut arr = Array2::from_elem((n_row, n_col), Complex32::default());
+    
+    let mut real: [u8; 4] = [0u8; 4]; 
+    let mut imag: [u8; 4] = [0u8; 4]; 
+
+    let data = image_header.read_data_bytes(..); // read all the data
+    let data_chunks = &mut data.chunks(4);  // grab 4 bytes at a time 
+
+    for row in arr.rows_mut() {
+        for elm in row {
+            real.copy_from_slice(data_chunks.next().unwrap());
+            imag.copy_from_slice(data_chunks.next().unwrap());
+
+            *elm = Complex32{
+                re: f32::from_be_bytes(real),
+                im: f32::from_be_bytes(imag)
+            };
+        }
+    }
+    return arr
 }
 
 // UNIT TESTS
