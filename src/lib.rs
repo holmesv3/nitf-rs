100d5abd8fc1846217323bd169f4a49097dd106a
Vaughn Holmes <vaughnhlms@hotmail.com>
2023-05-21 15:45:10 -0400

    Added docstring

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,9 @@
 //! Crate for reading and manipulating NITF files
-//!
-
-// TODO:
-// Need to implement reading optional segments
-// Meaning: after the main header, each sub-segment starts with 2 characters.
-// Need to implement reading these in some arbitrary order, storing starting bytes, offsets, etc.
-
 use std::fs::File;
 use std::path::Path;
 
 use ndarray::Array2;
-use nitf_2_1::headers::image::ImageHeader;
+use nitf_2_1::subheaders::image::ImageHeader;
 use nitf_2_1::types::DataSegment;
 use num_complex::Complex32;
 
@@ -21,16 +14,14 @@ use nitf_2_1::Nitf;
 /// Construct a `Nitf` object from a file `path`.
 ///
 /// If `path` is `None`, returns `Nitf::default()`
-/// 
+///
 /// # Example
-/// 
+///
 ///     use std::path::Path;
 ///     use nitf_rs::read_nitf;
-/// 
-///     fn main() {
-///     
-///     }
-/// 
+///
+///     let nitf_path = Path::new(<path-to-file>);
+///     let nitf = read_nitf(Some(nitf_path));
 pub fn read_nitf(path: Option<&Path>) -> Nitf {
     match path {
         Some(path) => {
@@ -42,21 +33,29 @@ pub fn read_nitf(path: Option<&Path>) -> Nitf {
 }
 
 // TODO: Implement reading a slice of the data into an array, as opposed to the whole thing.
+// TODO: Support various data types based on header information
 /// Read image data from `image_header` into an array
 ///
-/// Only supports `Complex32` data
-pub fn data_to_array(
-    image_header: &DataSegment<ImageHeader>,
-) -> Array2<Complex32> {
-    let n_row: usize = image_header.meta.NROWS.string.parse().unwrap();
-    let n_col: usize = image_header.meta.NCOLS.string.parse().unwrap();
+/// Only supports `Complex32` data at the moment
+/// 
+/// # Example
+///
+///     use std::path::Path;
+///     use nitf_rs::{read_nitf, data_to_array};
+///
+///     let nitf_path = Path::new(<path-to-file>);
+///     let nitf = read_nitf(Some(nitf_path));
+///     let data = data_to_array(&nitf.image_segments[0]);
+pub fn data_to_array(image_segment: &DataSegment<ImageHeader>) -> Array2<Complex32> {
+    let n_row: usize = image_segment.meta.NROWS.string.parse().unwrap();
+    let n_col: usize = image_segment.meta.NCOLS.string.parse().unwrap();
 
     let mut arr = Array2::from_elem((n_row, n_col), Complex32::default());
 
     let mut real: [u8; 4] = [0u8; 4];
     let mut imag: [u8; 4] = [0u8; 4];
 
-    let data = image_header.read_data_bytes(..); // read all the data
+    let data = image_segment.read_data_bytes(..); // read all the data
     let data_chunks = &mut data.chunks(4); // grab 4 bytes at a time
 
     for row in arr.rows_mut() {
