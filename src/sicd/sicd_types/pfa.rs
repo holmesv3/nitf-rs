aa7aa4a10b7131bfcb380b8eccc03da6d5b208db
Vaughn Holmes <vaughnhlms@hotmail.com>
2023-06-04 19:03:04 -0400

    Added test for pfa

--- src/sicd/sicd_types/pfa.rs
+++ src/sicd/sicd_types/pfa.rs
@@ -20,3 +20,22 @@ pub struct STDeskew {
     pub Applied: bool,
     pub STDSPhasePoly: Poly2D,
 }
+
+#[cfg(test)]
+mod tests {
+    use super::Pfa;
+    use serde_xml_rs::from_str;
+
+    #[test]
+    fn test_pfa() {
+        let xml_str = r#"<PFA><FPN><X>0</X><Y>0</Y><Z>0</Z></FPN><IPN><X>0</X>
+            <Y>0</Y><Z>0</Z></IPN><PolarAngRefTime>0</PolarAngRefTime>
+            <PolarAngPoly order1="0"><Coef exponent1="0">0</Coef></PolarAngPoly>
+            <SpatialFreqSFPoly order1="0"><Coef exponent1="0">0</Coef>
+            </SpatialFreqSFPoly><Krg1>0</Krg1><Krg2>0</Krg2><Kaz1>0</Kaz1><Kaz2>
+            0</Kaz2></PFA>"#;
+        assert!(match from_str::<Pfa>(xml_str) {
+            Ok(_) => true, Err(_) => false
+        })
+    }
+}
